24371a9562776a21df0beae6390853e86e588884
“samdavid2022” <samueldavidson2022@gmail.com>
2022-03-10 07:09:33 +0000

    msdhoni-samuel-commit two

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -23,8 +23,7 @@ System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");
 }  
 System.out.println();  
 }  
-}  
-//driver code  
+}    
 public static void main(String args[])  
 {  
 Scanner sc = new Scanner(System.in);  
